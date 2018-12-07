7455f12706e480fb3a24976aa0db62a5f614c63a
William Luke <william@ordino.ai>
2018-12-07 18:20:19 +0300

    AutoImport Setting to Be Implemeted Latter

--- src/settings.ts
+++ src/settings.ts
@@ -5,10 +5,12 @@ import { config } from './editor';
 export const shouldBeConsideredJsFiles = (...files) => {
     const extentionsToBeConsideredJS = config().jsFilesExtensions;
     return files.every(file => extentionsToBeConsideredJS.includes(path.extname(file).replace('.', '')));
-}
+};
 
-export const commonJSModuleSystemUsed = () => config().jsModuleSystem === 'commonjs'
+export const commonJSModuleSystemUsed = () => config().jsModuleSystem === 'commonjs';
 
+// TODO Implement AutoImport Setting
+export const shouldAutoImport = () => config().autoImport === true;
 
 export const esmModuleSystemUsed = () => config().jsModuleSystem === 'esm';
 
