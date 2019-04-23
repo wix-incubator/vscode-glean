8d104f43b36a09a3741e25bb6f193ba49e71d196
timcluyts <Tcluyts@gmail.com>
2019-04-23 11:10:22 +0200

    fix(general): Fixes #66

--- src/editor.ts
+++ src/editor.ts
@@ -2,7 +2,6 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import { QuickPickItem } from 'vscode';
 
-
 export const workspaceRoot = () => vscode.workspace.rootPath;
 
 export const activeURI = () => vscode.window.activeTextEditor.document.uri;
@@ -15,62 +14,63 @@ export const activeEditor = () => vscode.window.activeTextEditor;
 export const config = () => vscode.workspace.getConfiguration('glean');
 
 export function currentEditorPath(): string {
-  const activeEditor = vscode.window.activeTextEditor;
-  if (!activeEditor) return;
+	const activeEditor = vscode.window.activeTextEditor;
+	if (!activeEditor) return;
 
-  const currentFilePath = path.dirname(activeEditor.document.fileName);
-  const rootMatcher = new RegExp(`^${workspaceRoot()}`);
-  const relativeCurrentFilePath = currentFilePath.replace(rootMatcher, '');
+	const currentFilePath = path.dirname(activeEditor.document.fileName);
+	const rootMatcher = new RegExp(`^${workspaceRoot()}`);
+	const relativeCurrentFilePath = currentFilePath.replace(rootMatcher, '');
 
-  return relativeCurrentFilePath;
+	return relativeCurrentFilePath;
 }
 
 export function openFile(absolutePath: string): PromiseLike<string> {
-  return vscode.workspace.openTextDocument(absolutePath)
-    .then((textDocument) => {
-      if (textDocument) {
-        vscode.window.showTextDocument(textDocument);
-        return absolutePath;
-      } else {
-        throw Error('Could not open document');
-      }
-    });
+	return vscode.workspace.openTextDocument(absolutePath).then(textDocument => {
+		if (textDocument) {
+			vscode.window.showTextDocument(textDocument);
+			return absolutePath;
+		} else {
+			throw Error('Could not open document');
+		}
+	});
 }
 
 export function selectedText() {
-  const editor = vscode.window.activeTextEditor;
-  const selection = editor.selection;
-  return editor.document.getText(selection);
+	const editor = vscode.window.activeTextEditor;
+	if (editor) {
+		const selection = editor.selection;
+		return editor.document.getText(selection);
+	} else {
+		return null;
+	}
 }
 
 export function allText() {
-  const editor = vscode.window.activeTextEditor;
-  return editor.document.getText();
+	const editor = vscode.window.activeTextEditor;
+	return editor.document.getText();
 }
 
 export function showInputBox(defaultValue, placeHolder) {
-  return vscode.window.showInputBox({
-    value: defaultValue,
-    placeHolder
-  });
+	return vscode.window.showInputBox({
+		value: defaultValue,
+		placeHolder
+	});
 }
-
 
 export function showQuickPicksList(choices: QuickPickItem[], placeHolder = '') {
-  return vscode.window.showQuickPick<vscode.QuickPickItem>(choices, {
-    placeHolder
-  })
-};
-
-export const convertRelativeToFullPath = relativePath => {
-  const root = workspaceRoot();
-  return root ? path.join(root, relativePath) : relativePath;  
+	return vscode.window.showQuickPick<vscode.QuickPickItem>(choices, {
+		placeHolder
+	});
 }
 
+export const convertRelativeToFullPath = relativePath => {
+	const root = workspaceRoot();
+	return root ? path.join(root, relativePath) : relativePath;
+};
+
 export const extractQuickPickValue = selection => {
-  if (!selection)
-    return;
-  return selection.label;
+	if (!selection) return;
+	return selection.label;
 };
 
 export const toQuickPick = (label: string, description?) => ({ label, description });
@@ -79,4 +79,5 @@ export const toQuickPicksList = (choices: string[]) => choices.map(item => toQui
 
 export const showErrorMessage = message => vscode.window.showErrorMessage(message);
 
-export const showInformationMessage = (message, items: string[] = []) => vscode.window.showInformationMessage(message, ...items);
+export const showInformationMessage = (message, items: string[] = []) =>
+	vscode.window.showInformationMessage(message, ...items);
